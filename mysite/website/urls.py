513b0cc5d349add3d0d92664f7b231f5bc1f8bc4
shag527 <shagunkatoch527@gmail.com>
2021-03-20 16:55:49 +0530

    Result function added

--- mysite/website/urls.py
+++ mysite/website/urls.py
@@ -3,4 +3,5 @@ from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('res/', views.result, name='result'),
 ]
--- mysite/website/views.py
+++ mysite/website/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import joblib
 
 def home(request):
     return render(request, 'home.html', {})
+
+def result(request):
+    model=joblib.load('final_model.sav')
+    per_depress=0
+    return render(request,'result.html',{'per_depress':per_depress})
